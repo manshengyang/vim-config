ba7775678a3865ad105215e97805823395a3756c
Mansheng Yang <mansheng.yang@gmail.com>
2015-07-17 15:09:54 +0800

    update

--- scripts/setup_submodules.sh
+++ scripts/setup_submodules.sh
@@ -1,7 +1,10 @@
 git submodule update --init --recursive
 git submodule foreach git pull origin master
 
+TOP=$(git rev-parse --show-toplevel)
 # compile command+t
-cd "$(git rev-parse --show-toplevel)"
+cd $TOP
 cd ./submodules/command-t/ruby/command-t && ruby extconf.rb && make
-cd "$(git rev-parse --show-toplevel)"
+cd $TOP
+cd ./submodules/you-complete-me && ./install.sh
+cd $TOP
