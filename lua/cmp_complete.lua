88b41d60b878d5f7a44392b5ebef4b8bdc342786
Mansheng Yang <mansheng.yang@gmail.com>
2023-05-19 15:34:27 +0800

    update cmp sorting

--- lua/cmp_complete.lua
+++ lua/cmp_complete.lua
@@ -51,6 +51,19 @@ cmp.setup({
       })
     }),
   },
+  sorting = {
+    comparators = {
+      cmp.config.compare.locality,
+      cmp.config.compare.offset,
+      cmp.config.compare.exact,
+      cmp.config.compare.score,
+      cmp.config.compare.recently_used,
+      -- cmp.config.compare.kind,
+      -- cmp.config.compare.sort_text,
+      -- cmp.config.compare.length,
+      -- cmp.config.compare.order,
+    },
+  },
 })
 
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
