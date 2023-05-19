3fbb54124321b756d69f059544e2371c7426440e
Mansheng Yang <mansheng.yang@gmail.com>
2023-05-19 15:44:21 +0800

    update cmp sorting

--- lua/cmp_complete.lua
+++ lua/cmp_complete.lua
@@ -54,11 +54,11 @@ cmp.setup({
   sorting = {
     comparators = {
       cmp.config.compare.locality,
-      cmp.config.compare.offset,
-      cmp.config.compare.exact,
       cmp.config.compare.score,
-      cmp.config.compare.recently_used,
-      -- cmp.config.compare.kind,
+      cmp.config.compare.kind,
+      -- cmp.config.compare.offset,
+      -- cmp.config.compare.exact,
+      -- cmp.config.compare.recently_used,
       -- cmp.config.compare.sort_text,
       -- cmp.config.compare.length,
       -- cmp.config.compare.order,
